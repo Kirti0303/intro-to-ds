d6c9a2a8b3f044aad558d7e6559264cae6dc693d
Kirti0303 <Kirti0303@users.noreply.github.com>
2020-01-28 14:15:02 +0530

    Code file created.

---
+++ kirti0303/code.py
@@ -0,0 +1,33 @@
+# --------------
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+
+# print the first 5 rows of the dataset
+
+# Split the data into independent and target variable
+
+# Split the data into train and test data sets
+
+
+
+# --------------
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+# Assign the Randomforrest classifier to a variable rf
+
+# Train the model
+
+# Predict the class on the test data
+
+
+
+# --------------
+from sklearn.metrics import accuracy_score,mean_absolute_error
+
+# Accuracy score
+
+
+
